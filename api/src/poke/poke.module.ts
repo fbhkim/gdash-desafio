7f77133678fa52510cd9a17431efff67ec73a838
Felipe Kim <felipe.kim@pathbit.com.br>
2025-12-01 17:56:27 -0300

    Reset base: empty tree

--- api/src/poke/poke.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { PokeController } from './poke.controller'
-
-@Module({
-  controllers: [PokeController]
-})
-export class PokeModule {}
--- frontend/src/ui/input.tsx
+++
@@ -1,4 +0,0 @@
-type Props = { className?: string } & { [key: string]: any }
-export function Input({ className, ...props }: Props) {
-  return <input className={(className ? className + ' ' : '') + 'border rounded px-3 py-2 text-sm focus:outline-none focus:ring focus:border-gray-400'} {...props} />
-}
